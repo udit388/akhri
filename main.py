ac7d6c1221908dceb1dd046c2b19a0fe204467a7
udit388 <157692532+udit388@users.noreply.github.com>
2025-11-04 19:08:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 import time
 
-# 🎞 Google Drive video link (converted to direct link)
+# 🎞 Google Drive video link
 VIDEO_URL = "https://drive.google.com/uc?export=download&id=1TDKD9ZrzdhqcjUqai5-TeHRadmVsLKdg"
 
-# 🔑 YOUTUBE STREAM KEY (apni nayi key yaha daalo)
-YOUTUBE_STREAM_KEY = "z33m-9y0h-z7w0-4qhy-9vgd"
+# 🔑 YouTube Stream Key (environment variable se lega)
+YOUTUBE_STREAM_KEY = os.environ.get("YOUTUBE_STREAM_KEY")
 
+if not YOUTUBE_STREAM_KEY:
+    raise SystemExit("⚠️ Set YOUTUBE_STREAM_KEY in Render Environment Variables.")
+
+# 🔁 Loop stream forever
 while True:
-    os.system(f'ffmpeg -re -stream_loop -1 -i "{VIDEO_URL}" -c:v libx264 -c:a aac -f flv "rtmp://a.rtmp.youtube.com/live2/{z33m-9y0h-z7w0-4qhy-9vgd}"')
+    os.system(f'ffmpeg -re -stream_loop -1 -i "{VIDEO_URL}" -c:v libx264 -c:a aac -f flv "rtmp://a.rtmp.youtube.com/live2/{YOUTUBE_STREAM_KEY}"')
     time.sleep(5)
+
+
