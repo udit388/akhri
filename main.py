f1846498d7ec8ea90dc5284cadefa3d723865773
udit388 <157692532+udit388@users.noreply.github.com>
2025-11-05 18:30:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import os
 import time
 
-# 🎞 Dropbox video link (direct link form)
-VIDEO_URL = "https://www.dropbox.com/scl/fi/ym9z7i7qupuf2vc1x8hhb/ram.mp4.mp4?rlkey=io2vz85n3rh6psjdh0g15afqi&st=ut3bo8im&dl=1"
-
-# 🔑 YouTube Stream Key (Render Environment Variable se lega)
-YOUTUBE_STREAM_KEY = os.environ.get("YOUTUBE_STREAM_KEY")
+VIDEO_URL = "https://drive.google.com/uc?export=download&id=1TDKD9ZrzdhqcjUqai5-TeHRadmVsLKdg"
+YOUTUBE_STREAM_KEY = os.getenv("YOUTUBE_STREAM_KEY")
 
 if not YOUTUBE_STREAM_KEY:
-    raise SystemExit("⚠️ Please set YOUTUBE_STREAM_KEY in Render Environment Variables.")
+    raise SystemExit("⚠️ Set YOUTUBE_STREAM_KEY in Environment Variables!")
 
-# 🔁 Stream loop
 while True:
-    os.system(
-        f'ffmpeg -re -stream_loop -1 -i "{VIDEO_URL}" '
-        f'-c:v libx264 -c:a aac -f flv '
-        f'"rtmp://a.rtmp.youtube.com/live2/{YOUTUBE_STREAM_KEY}"'
-    )
+    os.system(f'ffmpeg -re -stream_loop -1 -i "{VIDEO_URL}" -c:v libx264 -c:a aac -f flv "rtmp://a.rtmp.youtube.com/live2/{YOUTUBE_STREAM_KEY}"')
     time.sleep(5)
